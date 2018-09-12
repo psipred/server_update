911b2ec5ed1a576631b1e16fb37a27189887b880
Daniel Buchan <clone@myself.com>
2018-09-12 10:10:12 +0100

    tdb update script

--- update_pdb_staging.sh
+++ update_pdb_staging.sh
@@ -1,3 +1,21 @@
-cd /webdata/data/pdb;
-rsync -rlpt -v -z --delete rsync.ebi.ac.uk::pub/databases/pdb/data/structures/divided/pdb/ /webdata/data/pdb/
-mv */* ./
+#!/bin/#!/usr/bin/env bash
+trap "echo Exited!; exit;" SIGINT SIGTERM
+
+MAX_RETRIES=300
+i=0
+
+# Set the initial return value to failure
+
+cd /webdata/data/pdb/ftp.wwpdb.org/pub/pdb/data/structures/all/pdb
+false
+while [ $? -ne 0 -a $i -lt $MAX_RETRIES ]
+do
+#echo "HEY"
+i=$(($i+1))
+rsync -rLpt -v -z --delete rsync.ebi.ac.uk::pub/databases/msd/pdb_uncompressed/ /webdata/data/pdb/ftp.wwpdb.org/pub/pdb/data/structures/all/pdb
+done
+
+if [ $i -eq $MAX_RETRIES ]
+then
+echo "Hit maximum number of retries, giving up."
+fi
