2d62852ee7529d62eeb24db5acfb80b993caa679
Daniel Buchan <clone@myself.com>
2019-02-20 15:29:51 +0000

    change pdb update

--- update_pdb_staging.sh
+++ update_pdb_staging.sh
@@ -20,14 +20,14 @@ then
 echo "Hit maximum number of retries, giving up."
 fi
 
-while [ $? -ne 0 -a $i -lt $MAX_RETRIES ]
-do
-#echo "HEY"
-i=$(($i+1))
-ssh django_worker@bioinfstage4 rsync -rLpt -v -z --delete rsync.ebi.ac.uk::pub/databases/msd/pdb_uncompressed/ /webdata/data/pdb/ftp.wwpdb.org/pub/pdb/data/structures/all/pdb
-done
-
-if [ $i -eq $MAX_RETRIES ]
-then
-echo "Hit maximum number of retries, giving up."
-fi
+# while [ $? -ne 0 -a $i -lt $MAX_RETRIES ]
+# do
+# #echo "HEY"
+# i=$(($i+1))
+# ssh django_worker@bioinfstage4 rsync -rLpt -v -z --delete rsync.ebi.ac.uk::pub/databases/msd/pdb_uncompressed/ /webdata/data/pdb/ftp.wwpdb.org/pub/pdb/data/structures/all/pdb
+# done
+#
+# if [ $i -eq $MAX_RETRIES ]
+# then
+# echo "Hit maximum number of retries, giving up."
+# fi
