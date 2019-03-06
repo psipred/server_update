b1f0dd8ce7e2796b64262be6136af03591d84b0a
Daniel Buchan <clone@myself.com>
2019-03-06 11:30:44 +0000

    remove rogue old hhblitsdb.pl tmp files

--- tidy_tmp.sh
+++ tidy_tmp.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-find /tmp -maxdepth 1 -type d -ctime +10 -regextype posix-extended -regex ".*.{8}-.{4}-.{4}-.{4}-.{12}" -exec rm -rf "{}" \;
+find /tmp -maxdepth 1 -type d -ctime +3 -regextype posix-extended -regex ".*.{8}-.{4}-.{4}-.{4}-.{12}" -exec rm -rf "{}" \;
+find /tmp/django_worker/ -maxdepth 1 -type d -ctime +2 -exec rm -rf "{}" \;
