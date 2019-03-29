90942cd8ff87ef5729cfd7d4704c406da6596d89
Daniel Buchan <clone@myself.com>
2019-03-29 15:20:02 +0000

    production tdb update completed

--- update_tdb.sh
+++ update_tdb.sh
@@ -44,8 +44,9 @@ chmod uog+rw /data/update_tdb/cullpdb.lst
 /data/maketdb/bin/make_tdb.pl -i /data/update_tdb/cullpdb.lst -o /data/update_tdb/psichain.lst -d /data/update_tdb/dssp/ -s /data/server_update/src/dsspcmbi -t /data/pgenthreader/tdb/ -u /data/uniref/unirefmain.fasta -b /data/ncbi-blast-2.7.1+/bin/psiblast -v 0.001 -h 2 -m /data/maketdb/src/chkparse -c /data/T-COFFEE_distribution_Version_11.00.8cbe486/bin/binaries/linux/t_coffee -p /data/pdb/ -q /data/cath_data/cath-domain-list-v4_2_0_annotated.txt -r /data/update_tdb/ -f /data/update_tdb/ -a /data/pgenthreader/cath_domain_tdb -y C -e /data/maketdb/bin/parse_source -l /data/cath_data/cath-domain-seqs-S100-v4_1_0.fa
 #./auto_psisum >& auto.log
 #### HERE ####
-chmod uog+rw /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/psichain.lst
-/bin/cp -f /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/psichain.lst /scratch1/NOT_BACKED_UP/dbuchan/foldlib/
+chmod uog+rw /data/update_tdb/psichain.lst
+/bin/cp -f /data/update_tdb/psichain.lst /data/pgenthreader/tdb/
+/bin/cp -f /data/update_tdb/psichain.lst /data/pgenthreader/data/
 
 if ($status != 0) then
     tail -50 auto.log | Mail -s AUTO_PSISUM-FAILED psipred@cs.ucl.ac.uk
@@ -61,8 +62,8 @@ if ($new_nl - $old_nl < -500) then
     exit
 endif
 
-cd /scratch1/NOT_BACKED_UP/dbuchan/foldlib/
-chmod uog+rw /scratch1/NOT_BACKED_UP/dbuchan/foldlib/*
+cd /data/pgenthreader/tdb/
+chmod uog+rw /data/pgenthreader/tdb/*
 #remove the old fasta file of all the chains
 /bin/rm -f psichain.fasta
 
@@ -79,7 +80,7 @@ foreach tdb (`cat psichain.lst`)
     echo $tdb.tdb >> tar.lst
 
 	#then add the tdb file's sequence to the new fastafile
-    /scratch0/NOT_BACKED_UP/dbuchan/Code/Update_scripts/src/tdb2fasta < $tdb.tdb >> psichain.fasta
+    /data/server_update/src/tdb2fasta < $tdb.tdb >> psichain.fasta
 end
 #exit 0
 chmod uog+rw psichain.fasta
@@ -87,16 +88,16 @@ tar zcf foldlib.tar.gz --files-from tar.lst
 
 #move the list of tdb to the old list for next week's update
 /bin/cp -f ./psichain.lst ./psichain.old
-/bin/cp -f ./psichain.lst /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/psichain.old
-chmod uog+rw /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/psichain.old
+/bin/cp -f ./psichain.lst /data/update_tdb/psichain.old
+chmod uog+rw /data/update_tdb/psichain.old
 
 #move a copy of the seq file to an old list
-/scratch0/NOT_BACKED_UP/dbuchan/Code/Update_scripts/src/remove_blanks.pl psichain.fasta > out.fasta
+/data/server_update/src/remove_blanks.pl psichain.fasta > out.fasta
 /bin/mv out.fasta psichain.fasta
 chmod uog+rw psichain.fasta
 
 /bin/cp -f ./psichain.fasta ./psichain.fasta.old
-/bin/cp -f ./psichain.fasta /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/psichain.fasta.old
+/bin/cp -f ./psichain.fasta /data/update_tdb/psichain.fasta.old
 
 #copy the list to the psipred dir and rsync to other worker hosts
 #
@@ -110,8 +111,7 @@ chmod uog+rw psichain.fasta
 #chmod uog+rw /webdata/data/current/psipred/data/psichain.lst
 
 #move list to public web site
-rsync /scratch1/NOT_BACKED_UP/dbuchan/foldlib tdb_sync@bioinfadmin:/var/www/html/downloads/pGenTHREADER/foldlibs/
-#/bin/cp -f psichain.lst /var/www/html/foldlib/
+/bin/cp -f psichain.lst /var/www/html/foldlib/
 
 # #copy the fasta file to the data dir
 # /bin/cp -f psichain.fasta /webdata/binaries/current/psipred/
@@ -123,26 +123,31 @@ rsync /scratch1/NOT_BACKED_UP/dbuchan/foldlib tdb_sync@bioinfadmin:/var/www/html
 # cd /webdata/data/current/blast/db/
 # /webdata/binaries/current/BLAST/bin/formatdb -i psichain.fasta
 #move fasta file to public website
-rsync /webdata/data/tdb/psichain.fasta tdb_sync@bioinfadmin:/var/www/html/downloads/pGenTHREADER/foldlibs/
+rsync /data/pgenthreader/tdb/psichain.fasta django_worker@bioinfadmin:/var/www/html/downloads/pGenTHREADER/foldlibs/
+scp /data/pgenthreader/tdb/psichain.fasta django_worker@bm3:/data/pgenthreader/tdb/
 #/bin/cp -f psichain.fasta /var/www/html/foldlib/
 
 #move the fold lib to the public data dir
-rsync /webdata/data/tdb/foldlib.tar.gz tdb_sync@bioinfadmin:/var/www/html/downloads/pGenTHREADER/foldlibs/
+rsync /data/pgenthreader/tdb/foldlib.tar.gz django_worker@bioinfadmin:/var/www/html/downloads/pGenTHREADER/foldlibs/
+scp /data/pgenthreader/tdb/foldlib.tar.gz django_worker@bm3:/data/pgenthreader/tdb/
+ssh django_worker@bm3 "cd /data/pgenthreader/tdb/; tar -zxvf foldlib.tar.gz"
 #/bin/mv -f foldlib.tar.gz /var/www/html/foldlib
-
+rsync /data/pgenthreader/tdb/psichain.lst django_worker@bioinfadmin:/var/www/html/downloads/pGenTHREADER/foldlibs/
+scp /data/pgenthreader/tdb/psichain.lst django_worker@bm3:/data/pgenthreader/tdb/
+scp /data/pgenthreader/tdb/psichain.lst django_worker@bm3:/data/pgenthreader/data/
 #create all the new images from the fold lib
 #No longer needed due to CATH image referencing.
 #csh /var/www/cgi-bin/psipred/bin/make_images
 
 # Clean the temp directory
-rm -f /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/*.fsa
-rm -f /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/*.chk
-rm -f /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/*.slx
-rm -f /scratch0/NOT_BACKED_UP/dbuchan/tdb_update/*.bls
+rm -f /data/update_tdb/*.fsa
+rm -f /data/update_tdb/*.chk
+rm -f /data/update_tdb/*.slx
+rm -f /data/update_tdb/*.bls
 # /usr/bin/find /webdata/tmp/autoupdate/ -mindepth 1 -mtime +60 -exec rm -rf {} \;
 
 # Inform psipred email address
-cd /webdata/data/autoupdate/
+cd /data/update_tdb
 wc -l ./psichain.lst | Mail -s AUTOUPDATE-OK psipred@cs.ucl.ac.uk
 
 # stop workers
