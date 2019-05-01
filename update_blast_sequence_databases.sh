1cdc2b890efdc535a31743b3dcece9436173b47d
Daniel Buchan <clone@myself.com>
2019-05-01 17:04:11 +0100

    correct blast queue start

--- update_blast_sequence_databases.sh
+++ update_blast_sequence_databases.sh
@@ -18,11 +18,11 @@ cd /home/blast_worker/analytics_automated/
 celery multi stop_verify worker --pidfile=celery.pid
 cp /data/update_tmp/pdbaa* /data/pdbaa/
 cp /data/update_tmp/uniref* /data/uniref/
-celery --app=analytics_automated_project.celery:app worker --loglevel=INFO -Q sequpdate,blast,low_blast,high_blast --detach --pidfile=celery.pid
+celery --app=analytics_automated_project.celery:app worker --loglevel=INFO -Q sequpdate,blast,low_blast,high_blast --concurrency=24 --detach --pidfile=celery.pid
 
 ssh blast_worker@bm1 "source /home/blast_worker/aa_env/bin/activate; celery multi stop_verify worker --pidfile=/home/blast_worker/analytics_automated/celery.pid"
 scp /data/update_tmp/pdbaa* blast_worker@bm1:/data/pdbaa/
 scp /data/update_tmp/uniref* blast_worker@bm1:/data/uniref/
-ssh blast_worker@bm1 "source /home/blast_worker/aa_env/bin/activate; cd /home/blast_worker/analytics_automated/; celery --app=analytics_automated_project.celery:app worker --loglevel=INFO -Q blast,low_blast,high_blast --detach --pidfile=celery.pid"
+ssh blast_worker@bm1 "source /home/blast_worker/aa_env/bin/activate; cd /home/blast_worker/analytics_automated/; celery --app=analytics_automated_project.celery:app worker --loglevel=INFO -Q blast,low_blast,high_blast --concurrency=24 --detach --pidfile=celery.pid"
 
 echo `uname -n` | Mail -s "BLASTUPDATE-OK" -r psipred@cs.ucl.ac.uk -S smtp="smtp.cs.ucl.ac.uk:25" psipred@cs.ucl.ac.uk
