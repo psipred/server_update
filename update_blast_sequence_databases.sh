870885a3116e2cb65fd6704c16a2badc678f50ec
Daniel Buchan <clone@myself.com>
2019-10-03 12:31:01 +0100

    corrected blast update things

--- update_blast_sequence_databases.sh
+++ update_blast_sequence_databases.sh
@@ -6,7 +6,8 @@ err_report() {
 trap err_report ERR
 
 cd /data/update_tmp
-
+rm pdbaa.gz
+rm uniref90.fasta.gz
 wget --timeout 120 http://dunbrack.fccc.edu/Guoli/culledpdb_hh/pdbaa.gz
 gunzip pdbaa.gz
 /opt/blast-2.2.26/bin/formatdb -i pdbaa -p T
