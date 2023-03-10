0f5406e8229bab5820711d5fafd535743e7f924a
Daniel Buchan <clone@myself.com>
2023-03-10 13:39:46 +0000

    change expiry time for user files

--- tidy_production_submissions.sh
+++ tidy_production_submissions.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-find /webdata/production_submissions -type f -ctime +15 -exec rm {} \;
+# find /webdata/production_submissions -type f -ctime +15 -exec rm {} \;
+find /webdata/production_submissions -type f -ctime +7 -exec rm {} \;
 
 # echo `uname -n` | Mail -s "PRODUCTION SUBMISSION CLEANED" -r psipred@cs.ucl.ac.uk -S smtp="smtp.cs.ucl.ac.uk:25" psipred@cs.ucl.ac.uk
 #curl -X POST -H 'Content-type: application/json' --data '{"text":":smile:\n'`uname -n`' PRODUCTION SUBMISSION CLEANED"}' https://hooks.slack.com/services/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
