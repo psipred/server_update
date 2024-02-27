354713842dcafe778d43fd392958ebe30bb4e7b9
Daniel Buchan <clone@myself.com>
2024-02-27 17:17:04 +0000

    added memsat-svm-pore cleaning

--- tidy_tmp.sh
+++ tidy_tmp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 find /tmp -maxdepth 1 -type d -ctime +3 -regextype posix-extended -regex ".*.{8}-.{4}-.{4}-.{4}-.{12}" -exec rm -rf "{}" \;
 find /tmp/django_worker/ -maxdepth 1 -type d -ctime +2 -exec rm -rf "{}" \;
+find /data/memsat-svm-pore/input -type -f -mtime +30 delete
 
 # echo `uname -n` | Mail -s "TMP CLEANED" -r psipred@cs.ucl.ac.uk -S smtp="smtp.cs.ucl.ac.uk:25" psipred@cs.ucl.ac.uk
 curl -X POST -H 'Content-type: application/json' --data '{"text":":smile:\n'`uname -n`' TMP CLEANED"}' https://hooks.slack.com/services/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
